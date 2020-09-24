ee77ae1e4b322679ff2f434c2df71317dc1eedd8
申法宽 <sfk@live.cn>
2020-09-24 17:21:27 +0800

    tab

--- src/App.js
+++ src/App.js
@@ -1,11 +1,62 @@
 import React from 'react';
-import { Button } from 'antd-mobile';
 import logo from './logo.svg';
 import './App.css';
+import { Button, Tabs, WhiteSpace, Badge } from 'antd-mobile';
+
+const tabs = [
+  { title: <Badge text={'3'}>First Tab</Badge> },
+  { title: <Badge text={'今日(20)'}>Second Tab</Badge> },
+  { title: <Badge dot>Third Tab</Badge> },
+];
+
+const tabs2 = [
+  { title: 'First Tab', sub: '1' },
+  { title: 'Second Tab', sub: '2' },
+  { title: 'Third Tab', sub: '3' },
+];
+
+const TabExample = () => (
+  <div>
+    <Tabs tabs={tabs}
+      initialPage={1}
+      onChange={(tab, index) => { console.log('onChange', index, tab); }}
+      onTabClick={(tab, index) => { console.log('onTabClick', index, tab); }}
+    >
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '150px', backgroundColor: '#fff' }}>
+        Content of first tab
+      </div>
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '150px', backgroundColor: '#fff' }}>
+        Content of second tab
+      </div>
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '150px', backgroundColor: '#fff' }}>
+        Content of third tab
+      </div>
+    </Tabs>
+    <WhiteSpace />
+    <Tabs tabs={tabs2}
+      initialPage={1}
+      tabBarPosition="bottom"
+      renderTab={tab => <span>{tab.title}</span>}
+    >
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '150px', backgroundColor: '#fff' }}>
+        Content of first tab
+      </div>
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '150px', backgroundColor: '#fff' }}>
+        Content of second tab
+      </div>
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '150px', backgroundColor: '#fff' }}>
+        Content of third tab
+      </div>
+    </Tabs>
+    <WhiteSpace />
+  </div>
+);
 
 function App() {
   return (
     <div className="App">
+      <TabExample />
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -20,6 +71,7 @@ function App() {
           Learn React
         </a>
         <Button type="ghost" size="small" inline>small</Button>
+
       </header>
     </div>
   );
